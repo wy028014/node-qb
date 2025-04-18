dfeed387245c941af59e72db8bceed544f25ef5b
王野 <18545455617@163.com>
2025-04-18 08:23:07 +0000

    修复代码

--- background/src/modules/base/base.controller.ts
+++ background/src/modules/base/base.controller.ts
@@ -4,7 +4,7 @@ import { compare } from "bcryptjs";
 import { GroupOption, IncludeOptions, OrderItem, WhereOptions } from "sequelize";
 import { CaptchaObj } from "svg-captcha";
 import type { Response } from "express";
-import { Department, Person, User, UserType } from "#/models";
+import { Department, Person, User } from "#/models";
 import { CreateBaseDto } from "./dto/create-base.dto";
 import { BaseService } from "./base.service";
 import { DepartmentService } from "../department/department.service";
@@ -22,63 +22,67 @@ export class BaseController {
     private readonly personService: PersonService,
     private readonly userService: UserService,
   ) { }
-
   @ApiBody({ description: `登录信息`, isArray: false, type: CreateBaseDto })
   @ApiOperation({ summary: `登录接口` })
   @Post()
   async create(@Req() req, @Body() createBaseDto: CreateBaseDto): Promise<MyRes | HttpException> {
     if (!req.session.code || req.session.code.toLocaleLowerCase() !== createBaseDto.l_captcha!.toLocaleLowerCase()) {
       throw new HttpException(`验证码错误`, 400);
-    }
-
-    const where: WhereOptions<any> = {};
-    let attributes: any = undefined;
-    const include: IncludeOptions[] = [];
-    const order: OrderItem[] = [];
-    const group: GroupOption = [];
-    const limit: number = 1;
-    const offset: number = 0;
-    const paranoid: boolean = false;
-    const benchmark: boolean = true;
-    let id: string = ``;
-    let classification: string = ``;
-    attributes = [`uid`];
-
-    if (/^\d+$/.test(createBaseDto.l_username)) {
-      where.certificateNumber = createBaseDto.l_username;
-      const persons: Person[] = await this.personService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
-      if (!persons[0]) {
-        throw new HttpException(`没有找到用户`, 404);
-      }
-      id = persons[0].dataValues.id;
-      classification = UserType.PERSON;
     } else {
-      where.name = createBaseDto.l_username;
-      const departments: Department[] = await this.departmentService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
-      if (!departments[0]) {
-        throw new HttpException(`没有找到部门`, 404);
+      const where: WhereOptions<any> = {};
+      let attributes: any = undefined;
+      const include: IncludeOptions[] = [];
+      const order: OrderItem[] = [];
+      const group: GroupOption = [];
+      const limit: number = 1;
+      const offset: number = 0;
+      const paranoid: boolean = false;
+      const benchmark: boolean = true;
+      let id: string = ``;
+      let classification: string = ``;
+      attributes = [`id`];
+      if (/^\d+$/.test(createBaseDto.l_username)) {
+        where.police_number = createBaseDto.l_username;
+        const persons: Person[] = await this.personService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
+        if (!persons[0]) {
+          throw new HttpException(`没有找到用户`, 404);
+        }
+        id = persons[0].dataValues.id;
+        classification = `person`;
+      } else {
+        const departments: Department[] = await this.departmentService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
+        where.name = createBaseDto.l_username;
+        if (!departments[0]) {
+          throw new HttpException(`没有找到部门`, 404);
+        }
+        id = departments[0].dataValues.id;
+        classification = `department`;
       }
-      id = departments[0].dataValues.id;
-      classification = UserType.DEPARTMENT;
+      delete where.police_number;
+      delete where.name;
+      where.classification = classification;
+      attributes = [`id`, `password`];
+      switch (classification) {
+        case `person`: {
+          where.personId = id;
+          break;
+        }
+        case `department`: {
+          where.departmentId = id;
+          break;
+        }
+      }
+      const users: User[] = await this.userService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
+      if (!users[0]) {
+        throw new HttpException(`不是合法的注册用户`, 404);
+      }
+      if (!(await compare(createBaseDto.l_password, users[0].dataValues.password))) {
+        throw new HttpException(`密码错误`, 404);
+      } else {
+        const token: { token_access: string; token_refresh: string } = await this.baseService.create(id, classification);
+        return { data: { userId: users[0].dataValues.id, id, classification, ...token }, message: `登录成功`, success: true };
+      }
     }
-
-    delete where.certificateNumber;
-    delete where.name;
-    where.classification = classification;
-    where.id = id;
-    attributes = [`uid`, `password`];
-
-    const users: User[] = await this.userService.find(where, attributes, include, order, group, limit, offset, paranoid, benchmark);
-    if (!users[0]) {
-      throw new HttpException(`不是合法的注册用户`, 404);
-    }
-
-    if (!(await compare(createBaseDto.l_password, users[0].dataValues.password))) {
-      throw new HttpException(`密码错误`, 404);
-    }
-
-    const token: { token_access: string; token_refresh: string } = await this.baseService.create(id, classification);
-    return { data: { userId: users[0].dataValues.uid, id, classification, ...token }, message: `登录成功`, success: true };
   }
 
   @ApiOperation({ summary: `获取验证码` })
